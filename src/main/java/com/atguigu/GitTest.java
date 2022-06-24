0e00ef11e8e6ef006f35e082d80ae96621869855
Awei <1021744820@qq.com>
2022-06-24 20:03:49 +0800

    dev提交

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("创建新文件");
         System.out.println("再次提交");
+        System.out.println("dev添加分支");
     }
 }
